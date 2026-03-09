e3f8dcb55ac0f7a55fe90cae94c3511ef31eb4d8
Evan Jackson <evan.jackson@torqit.ca>
2026-03-09 14:34:21 -0400

    More name updates

--- src/DependencyInjection/TorqNorthPimcoreHelpersExtension.php
+++ src/DependencyInjection/TorqNorthPimcoreHelpersExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class TorqPimcoreHelpersExtension extends Extension
+class TorqNorthPimcoreHelpersExtension extends Extension
 {
     /**
      * {@inheritdoc}
